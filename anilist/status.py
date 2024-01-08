7ccf6fafbfbf8cd91b0ccba4b9416f177b5bd077
Agsayan <agsayan0@gmail.com>
2024-01-08 13:40:10 +0100

    removed unused imports

--- anilist/status.py
+++ anilist/status.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 import enum
-from typing import List, dataclass_transform
 
 
 class MediaType(enum.Enum):
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 from anilist import Anilist
 from anilist.status import ReadingStatus
 
-TOKEN = os.environ["ANILIST_TOKEN"]
-anilist = Anilist(TOKEN)
+# TOKEN = os.environ["ANILIST_TOKEN"]
+anilist = Anilist()
 id_jjk = 101517
 search_query = "Nigoru Hitomi de Nani wo Negau: Haiseruku Senki"
 print(anilist.search_manga("i raised the villains preciously"))
