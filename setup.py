4fcf63346a3ba0a54910b39df75a20be2e8b1bec
Agsayan <agsayan0@gmail.com>
2024-09-08 14:32:22 +0200

    increased version number from 0.1.0 to 0.1.1

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup
-setup(
-    name='anilist',
-    version='0.1.0',
-    description='A Anilist API',
-    url='https://github.com/Agsayan/anilist',
-    author='Sayan',
-    author_email='',
-    license='BSD 2-clause',
-    packages=['anilist'],
-    install_requires=[
-        'requests',
-    ],
 
+setup(
+    name="anilist",
+    version="0.1.1",
+    description="A Anilist API",
+    url="https://github.com/Agsayan/anilist",
+    author="Sayan",
+    author_email="",
+    license="BSD 2-clause",
+    packages=["anilist"],
+    install_requires=[
+        "requests",
+    ],
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
 )
