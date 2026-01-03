839b3fd9785f2b3c86cdae502b76315db42ad639
Agsayan <agsayan0@gmail.com>
2026-01-03 16:36:54 +0100

    chore: minor version update (0.1.4)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="anilist",
-    version="0.1.3",
+    version="0.1.4",
     description="A Anilist API",
     url="https://github.com/Agsayan/anilist",
     author="Sayan",
